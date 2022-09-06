dc7bde42ef0bdd25274e2f06eddf31c341d90231
stillyslalom <aames@wisc.edu>
2022-09-06 12:09:14 -0500

    Fix show for shockcalc

--- src/ShockTube.jl
+++ src/ShockTube.jl
@@ -37,7 +37,7 @@ function shockreflect!(shocked, Ms)
     γ2 = γ(shocked)
     lhs = Ms/(Ms^2 - 1) * sqrt(1 + 2(γ2 - 1)/(γ2 + 1)^2 * (Ms^2 - 1) * (γ2 + 1/Ms^2))
     Mr = (1 + sqrt(1 +4*lhs^2))/(2lhs)
-    shockjump!(shocked, Mr)
+    first(shockjump!(shocked, Mr)), Mr
 end
 shockreflect(shocked, Ms) = shockreflect!(copy(shocked), Ms)
 
@@ -47,35 +47,39 @@ struct ShockCalc{DR<:PyThermo.Chemical,DV<:PyThermo.Chemical,U<:Unitful.Velocity
     shocked::DV
     reflected::DV
     Ms::Float64
+    Mr::Float64
     u2::U
 end
 
-function Base.show(io::IO, sc::ShockCalc)
+function showm(io::IO, m::MIME, sc::ShockCalc)
     ush(u, val) = @sprintf("%0.4g", ustrip(u, val))
     printstate(s) = join((ush(u"MPa", pressure(s)),
                           ush(u"K", temperature(s)),
                           ush(u"kg/m^3", density(s)),
                           ush(u"m/s", soundspeed(s))), " | ")
-    display(Markdown.parse("""
+    show(io, m, Markdown.parse("""
     | Region      |  P [MPa]  | T [K] | ρ [kg/m³] | cₛ [m/s] |
     |:----------- | :------------: | :-------------: | :-------------: | :---------------: |
     | Driver      | $(printstate(sc.driver)) |
     | Driven      | $(printstate(sc.driven)) |
     | Shocked     | $(printstate(sc.shocked)) |
     | Reflected   | $(printstate(sc.reflected)) |
+
+    Incident wave: $(ush(u"m/s", sc.Ms*soundspeed(sc.driven))) m/s (Mach $(@sprintf "%0.4g" sc.Ms))\\
+    Reflected wave: $(ush(u"m/s", sc.Mr*soundspeed(sc.shocked))) m/s (Mach $(@sprintf "%0.4g" sc.Mr))\\
+    Post-shock velocity: $(ush(u"m/s", sc.u2)) m/s
     """))
-    println()
-    println("Driver gas: ", PyThermo.composition_string(sc.driver))
-    println("Driven gas: ", PyThermo.composition_string(sc.driven))
-    print("Post-shock velocity: ", ush(u"m/s", sc.u2), " m/s")
 end
+
+Base.show(io::IO, sc::ShockCalc) = showm(io, MIME("text/plain"), sc)
+Base.show(io::IO, m::MIME"text/html", sc::ShockCalc) = showm(io, m, sc)
 
 function shockcalc!(driver, driven, Ms)
     shocked, u2 = shockjump(driven, Ms)
     driverpressure!(driver, driven, Ms)
-    reflected, _ = shockreflect(shocked, Ms)
+    reflected, Mr = shockreflect(shocked, Ms)
     # return (driver = driver, driven = driven, shocked=shocked, reflected=reflected, u2=u2)
-    return ShockCalc(driver, driven, shocked, reflected, Ms, u2)
+    return ShockCalc(driver, driven, shocked, reflected, Ms, Mr, u2)
 end
 shockcalc(driver, driven, Ms) = shockcalc!(copy(driver), driven, Ms)
 
